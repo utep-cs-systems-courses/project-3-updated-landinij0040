46e3b2f921572cedeaa06d6514bd03cf7fdb2d41
landinij0040 <ijlandin0040@miners.utep.edu>
2020-11-16 14:39:52 -0700

    wakedemo2.c makes the word drop down

--- wakedemo/wakedemo2.c
+++ wakedemo/wakedemo2.c
@@ -14,7 +14,7 @@ void wdt_c_handler()
   static int secCount = 0;
 
   secCount ++;
-  if (secCount == 250) {		/* once/sec */
+  if (secCount == 1) {		/* once/sec */
     secCount = 0;
     fontFgColor = (fontFgColor == COLOR_GREEN) ? COLOR_BLACK : COLOR_GREEN;
     redrawScreen = 1;
@@ -32,11 +32,13 @@ void main()
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
   
+  static int rowChange = 30;
   clearScreen(COLOR_BLUE);
   while (1) {			/* forever */
     if (redrawScreen) {
       redrawScreen = 0;
-      drawString8x12(30,10, "DROP ME", fontFgColor, COLOR_BLUE);
+      drawString8x12(30,rowChange, "DROP ME", fontFgColor, COLOR_BLUE);
+      rowChange += 2; // Adding two to make the Word go down 
     }
     P1OUT &= ~LED_GREEN;	/* green off */
     or_sr(0x10);		/**< CPU OFF */
