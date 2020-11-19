73d43dc7d0ab965fd7ece260535aeda5fa3b9888
landinij0040 <ijlandin0040@miners.utep.edu>
2020-11-19 11:46:20 -0700

    main displays blue

--- assembly_test/main.c
+++ assembly_test/main.c
@@ -1,9 +1,11 @@
 #include <msp430.h>
 #include <libTimer.h>
-#include <lcdutils.h>
-#include <lcddraw.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
 
 int main()
 {
+  configureClocks();
+  lcd_init();
   clearScreen(COLOR_BLUE);
 }
